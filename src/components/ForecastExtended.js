42b5ed2bb7a71b1a97153314a88a8fe1b8575f58
Andres Faya <afaya@mypartssource.com>
2020-03-24 10:09:10 -0300

    Final upload
    Final changes - tutorial finished

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,38 +1,92 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
+import { api_key, url_base_forecast } from './../constants/api_url';
+import transformForecast from './../services/transformForecast';
 
 import './styles.css';
 
-const days = [
-    'Lunes',
-    'Martes',
-    'Mièrcoles',
-    'Jueves',
-    'Viernes',
-];
+// const days = [
+//     'Lunes',
+//     'Martes',
+//     'Mièrcoles',
+//     'Jueves',
+//     'Viernes',
+// ];
 
-const data = {
-    temperature: '10',
-    humidity: 10,
-    weatherState: 'normal',
-    wind: '10'
-};
+// const data = {
+//     temperature: '10',
+//     humidity: 10,
+//     weatherState: 'normal',
+//     wind: '10'
+// };
 
 class ForecastExtended extends Component {
 
-    renderForecastItemDays() {
-        return days.map(day => (<ForecastItem weekDay={ day } hour={ 10 } data={ data }/>));
+    constructor() {
+        super();
+
+        this.state = {
+            forecastData: null
+        };
+    }
+
+    componentDidMount() {
+        const { city } = this.props;
+        this.updateCity(city);   
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { city } = this.props;
+
+        if (nextProps.city !== city) {
+            this.setState({ forecastData: null });
+            this.updateCity(nextProps.city);
+        }
+    }
+
+    updateCity = city => {
+        const url_forecast = `${url_base_forecast}?q=${ city }&appid=${api_key}`; 
+
+        // fecth or axios
+        fetch(url_forecast)
+            .then(
+                data => (data.json())
+            )
+            .then(
+                weather_data => {
+                    const forecastData = transformForecast(weather_data);
+                    this.setState({ forecastData });
+                }
+            );
+    }
+
+    renderForecastItemDays(forecastData) {
+        return forecastData.map(forecast => (
+            <ForecastItem 
+                key={`${forecast.weekDay}${forecast.hour}`}
+                weekDay={ forecast.weekDay } 
+                hour={ forecast.hour }
+                data={ forecast.data }
+                />));
+    }
+
+    renderProgress() {
+        return (<h3>Cargando Pronóstico Extendido...</h3>);
     }
 
     render() {
         const { city } = this.props;
+        const { forecastData } = this.state;
+
         return (
             <div>
                 <h2 className="forecast-title">
                     Pronóstico Extendido { city }
                 </h2>
-                { this.renderForecastItemDays() }
+                { forecastData ? 
+                    this.renderForecastItemDays(forecastData) :
+                    this.renderProgress() }
             </div>
         );
     }
--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,2 +1,3 @@
 export const api_key = "b3e27f12e6c2d937cfb1f864016a776d";
 export const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+export const url_base_forecast = "http://api.openweathermap.org/data/2.5/forecast";
